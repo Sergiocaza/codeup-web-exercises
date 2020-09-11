4992b1b87e3cb4580aa767f0373d16e6aa2ff04f
Sergio Cazaresmuniz <cazaresmuniz.sergio@gmail.com>
2020-09-11 11:01:31 -0500

    comepleted

---
+++ js/jquery_faq.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $('#answers').click(function(event) {
+        event.preventDefault();
+        $('.invisible').toggleClass('visible');
+    });
+});
+
+$(document).ready(function() {
+    $('ul').each(function(){
+        $(this).children('li').first().css('font-weight', 'bold');
+    });
+    $('#toggle1').click(function() {
+        $('#1').slideToggle();
+    });
+    $('#toggle2').click(function() {
+        $('#2').slideToggle();
+    });
+    $('#toggle3').click(function() {
+        $('#3').slideToggle();
+    });
+    $('#toggle4').click(function() {
+        $('#4').slideToggle();
+    });
+    $('#toggle5').click(function() {
+        $('#5').slideToggle();
+    });
+    $('#toggle6').click(function() {
+        $('#6').slideToggle();
+    });
+    $('#toggle7').click(function() {
+        $('#7').slideToggle();
+    });
+    $('#toggle8').click(function() {
+        $('#8').slideToggle();
+    });
+    $('#toggle9').click(function() {
+        $('#9').slideToggle();
+    });
+    $('#toggle10').click(function() {
+        $('#10').slideToggle();
+    });
+});
